62fb3b8294d0fe7a82608f3d9b679670c25334bf
Ednardo Gomes <ednardogomes08@hotmail.com>
2021-08-09 22:55:12 -0300

    Update comunidades.js

--- pages/api/comunidades.js
+++ pages/api/comunidades.js
@@ -2,7 +2,7 @@ import { SiteClient } from 'datocms-client';
 
 export default async function recebedorDeRequests(req, res) {
   if(req.method === 'POST') {
-    const TOKEN = '9621713e876bf55bb7812bd251767d';
+    const TOKEN = '';
     const client = new SiteClient(TOKEN);
 
     const registroCriado = await client.items.create({
